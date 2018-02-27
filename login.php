c3006b4dd774a2363eb77006ce3ebb7f6cce1446
Niel <nieloo.01@gmail.com>
2018-02-27 12:52:02 +0800

    Okay

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login Page</title>
+</head>
+<body>
+<fieldset>
+	<legend>Login</legend>
+<form method="POST" action="connectivity.php"> 
+	User </br>
+	<input type="text" name="user" size="40"></br>
+	Password </br>
+	<input type="password" name="pass" size="40"></br></br> 
+	<input id="button" type="submit" name="submit" value="Submit"> 
+</form>
+</fieldset>
+
+</body>
+</html>
